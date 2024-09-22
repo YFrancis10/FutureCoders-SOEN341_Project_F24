74fcec2e14ff20e0e4a5c49fd85c1cb45f8eaaad
Rami-66 <149551030+Rami-66@users.noreply.github.com>
2024-09-21 22:29:13 -0400

    Delete role-selection.js

--- role-selection.js
+++
@@ -1,9 +0,0 @@
-// role-selection.js
-document.getElementById('studentBtn').addEventListener('click', function() {
-    window.location.href = 'login.html?role=student';  // Redirect to the login screen for students
-  });
-  
-  document.getElementById('teacherBtn').addEventListener('click', function() {
-    window.location.href = 'login.html?role=teacher';  // Redirect to the login screen for teachers
-  });
-  
