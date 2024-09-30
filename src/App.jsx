c2eae2346b560069d48be8b6d359128eccfadba7
Akshey Visuvalingam <a_visuva@live.concordia.ca>
2024-09-30 07:35:33 -0400

    added the imports

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Signup from './Signup';
 import Login from './Login';
 import StudentDashboard from './Student_Dashboard'; 
 import TeacherDashboard from './Teacher_Dashboard';
+import Teams from './Teams';
 
 
 const App = () => {
@@ -25,6 +26,7 @@ const App = () => {
         <Route path="/Login" element={<Login />} />
         <Route path="/Student_Dashboard" element={<StudentDashboard />} />
         <Route path="/Teacher_Dashboard" element={<TeacherDashboard />} />
+        <Route path="/Teams" element={<Teams />} /> {/* Ensure this route exists */}
         <Route path="/" element={<Login />} />  {/* Redirect root to login */}
       </Routes>
     </Router>
