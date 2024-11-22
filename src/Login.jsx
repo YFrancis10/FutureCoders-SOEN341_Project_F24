d3af6eacd19d9f712fda8e2f39a302a32ce9066d
Akshey Visuvalingam <a_visuva@live.concordia.ca>
2024-11-22 16:18:15 -0500

    Changed form in login and signup

--- src/Login.jsx
+++ src/Login.jsx
@@ -42,7 +42,7 @@ const Login = () => {
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-auth-bg bg-cover bg-center">
-      <div className="bg-white rounded-lg shadow-lg p-8 w-full max-w-md">
+      <div className="bg-white rounded-lg shadow-lg p-8 w-full max-w-md glass">
         <h2 className="text-3xl font-bold mb-6 text-center text-gray-800">Log In</h2>
 
         {/* The form */}
