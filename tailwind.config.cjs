a5cf2aa22c5fd0b13f6731ff507d76a4ce1d18ea
Akshey Visuvalingam <a_visuva@live.concordia.ca>
2024-11-22 15:46:05 -0500

    added background image class to tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Adjust based on your file structure
+    "./src/**/*.{js,jsx,ts,tsx}", 
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'auth-bg': "url('/path/to/your/image.jpg')", 
+      },
+    },
   },
   plugins: [],
 };
+
