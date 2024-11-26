1721ec3aaf0b4cc5b7cf9f8caf0a775799c1b185
NicoLandry <NICOLASLANDRY03@GMAIL.COM>
2024-11-26 18:45:06 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Teams from './Teams';
 import Cooperation from './Cooperation';
 import PeerRating from './PeerRating';
 import Summary from './Summary';
+import EditTeam from './editTeam';
 import RoomList from './RoomList'; 
 import BookRoom from './BookRoom'; 
 import DetailedResults from './DetailedResults';
@@ -110,6 +111,7 @@ const App = () => {
         <Route path="/BookRoom/:roomId" element={<Layout pageTitle="Book Room"><BookRoom /></Layout>} />
         <Route path="/teams/:teamId/detailed-results" element={<Layout pageTitle="Detailed Results"><DetailedResults /></Layout>} />
         <Route path="/EditMeeting" element={<Layout pageTitle="Edit Meeting"><EditMeeting /></Layout>} />
+        <Route path="/editTeam/:teamId" element={<Layout pageTitle="Edit Team"><EditTeam /></Layout>} />
         {/* <Route path="/your-comments" element={<Layout pageTitle="Your Comments"><YourComments /></Layout>} /> */}
 
         {/* Profile route */}
